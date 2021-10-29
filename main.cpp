085b5062078ef1d0ac4e5982a44c7d46268bfbdb
20181185신준원 <83389447+G00dog@users.noreply.github.com>
2021-10-29 01:58:59 +0000

    HelloSDL 분리하기

--- main.cpp
+++ main.cpp
@@ -2,32 +2,50 @@
 
 SDL_Window* g_pWindow = 0;
 SDL_Renderer* g_pRenderer = 0;
+bool g_bRunning = false;
 
-int main(int argc, char* args[])
+bool init(const char* title, int xpos, int ypos, int width, int height, int flags)
 {
- 
-   if (SDL_Init(SDL_INIT_EVERYTHING) >= 0) 
-   {
-        g_pWindow = SDL_CreateWindow("Setting up SDL 10/22",
-            SDL_WINDOWPOS_CENTERED,
-            SDL_WINDOWPOS_CENTERED,
-            640, 480, SDL_WINDOW_SHOWN);
+  if (SDL_Init(SDL_INIT_EVERYTHING)>= 0)
+  {
+    g_pWindow = SDL_CreateWindow(title, xpos, ypos, width, height, flags);
 
-        if (g_pWindow != 0) {
-            g_pRenderer = SDL_CreateRenderer(g_pWindow, -1, 0);
-        }
-
+    if(g_pWindow != 0)
+    {
+      g_pRenderer = SDL_CreateRenderer(g_pWindow, -1, 0);
     }
-    else {
-        return 1;
-    }
+  }
+  else
+  {
+    return false;
+  }
 
-    SDL_SetRenderDrawColor(g_pRenderer, 0, 0, 0, 255);
-    SDL_RenderClear(g_pRenderer);
-    SDL_RenderPresent(g_pRenderer);
+  SDL_SetRenderDrawColor(g_pRenderer, 0, 0, 0, 255);
 
-    SDL_Delay(5000);
-    SDL_Quit();
-
-    return 0;
+  return true;
 }
+
+void render()
+{
+  SDL_RenderClear(g_pRenderer);
+  SDL_RenderPresent(g_pRenderer);
+}
+
+int main(int argc, char* argv[])
+{
+  if (init("Breaking Up HelloSDL", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+  640, 480, SDL_WINDOW_SHOWN))
+  {
+    g_bRunning = true;
+  }
+  else
+  {
+    return 1; 
+  }
+  while (g_bRunning)
+  {
+    render();
+  }
+  SDL_Quit();
+  return 0;
+} 
